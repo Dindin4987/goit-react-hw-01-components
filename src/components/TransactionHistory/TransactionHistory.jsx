6a154a189d7c91e517b8753b574daeca275de1b5
Dindin4987 <144334821+Dindin4987@users.noreply.github.com>
2024-05-19 16:04:06 +0800

    UPDATE

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,23 +1,37 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
 export const TransactionHistory = ({ items }) => {
   return (
-    <table class="transaction-history">
+    <table className={css.transactionHistory}>
       <thead>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={css.tableHeader}>Type</th>
+          <th className={css.tableHeader}>Amount</th>
+          <th className={css.tableHeader}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {items.map(item => (
-          <tr>
-            <td>Invoice</td>
-            <td>125</td>
-            <td>USD</td>
+          <tr className={css.tableRow} key={item.id}>
+            <td className={css.tableData}>{item.type}</td>
+            <td className={css.tableData}>{item.amount}</td>
+            <td className={css.tableData}>{item.currency}</td>
           </tr>
         ))}
       </tbody>
     </table>
   );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired
+    })
+  ).isRequired,
 };
