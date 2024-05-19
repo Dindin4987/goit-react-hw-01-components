dbfaedebcf23d4f94d30c188c7cb7bde800fbd38
Dindin4987 <144334821+Dindin4987@users.noreply.github.com>
2024-05-19 16:30:14 +0800

    update

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -15,14 +15,3 @@ export const FriendList = ({ friends }) => {
     </ul>
   );
 };
-
-FriendList.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
-      id: PropTypes.number.isRequired,
-    })
-  ).isRequired,
-};
